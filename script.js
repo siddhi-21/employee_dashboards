27d1c96e2d3072422b6872d60e59d9834861bb3b
siddhi-21 <81239925+siddhi-21@users.noreply.github.com>
2023-09-15 17:25:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,600 @@
+let data = [
+    {
+        "id": "1",
+        "email": "george.bluth@cmss.in",
+        "name": "George Bluth",
+        "dept": "Admin",
+        "designation": "Admin Exe",
+        "phone": "9869123456/0223451235",
+        "image": "img/ladiesImg/lady1.jpg",
+        "DOB": "1993-10-07",
+        "DOJ": "2022-12-14"
+    },
+    {
+        "id": "2",
+        "email": "janet.weaver@cmss.in",
+        "name": "Janet Weaver",
+        "dept": "Accounts",
+        "designation": "Accountant",
+        "phone": "9869123456/0223451235",
+        "image": "img/gentlemenImg/man1.jpg",
+        "DOB": "2000-02-23",
+        "DOJ": "2021-08-23"
+    },
+    {
+        "id": "3",
+        "email": "emma.wong@cmss.in",
+        "name": "Emma Wong",
+        "dept": "HR",
+        "designation": "Hr Exe",
+        "phone": "9869123456/0223451235",
+        "image": "img/ladiesImg/lady2.jpg",
+        "DOB": "1993-09-19",
+        "DOJ": "2023-01-21"
+    },
+    {
+        "id": "4",
+        "email": "eve.holt@cmss.in",
+        "name": "Eve Holt",
+        "dept": "IT",
+        "designation": "Senior Dev",
+        "phone": "9869123456/0223451235",
+        "image": "img/gentlemenImg/man2.jpg",
+        "DOB": "1994-03-15",
+        "DOJ": "2021-03-01"
+
+    },
+    {
+        "id": "5",
+        "email": "charles.morris@cmss.in",
+        "name": "Charles Morris",
+        "dept": "IT",
+        "designation": "Junior Dev",
+        "phone": "9869123456/0223451235",
+        "image": "img/ladiesImg/lady3.jpg",
+        "DOB": "2002-09-17",
+        "DOJ": "2023-09-19"
+    },
+    {
+        "id": "6",
+        "email": "tracey.ramos@cmss.in",
+        "name": "Tracey Ramos",
+        "dept": "Accounts",
+        "designation": "Operations Manager",
+        "phone": "9869123456/0223451235",
+        "image": "img/gentlemenImg/man3.jpg",
+        "DOB": "1993-09-15",
+        "DOJ": "2023-09-21"
+    },
+    {
+        "id": "7",
+        "email": "niki.sen@cmss.in",
+        "name": "Niki Sen",
+        "dept": "Admin",
+        "designation": "Administrative Head",
+        "phone": "9869123456/0223451235",
+        "image": "img/ladiesImg/lady4.jpg",
+        "DOB": "2003-01-18",
+        "DOJ": "2023-12-21"
+    },
+    {
+        "id": "8",
+        "email": "jacy.deo@cmss.in",
+        "name": "jacy Deo",
+        "dept": "IT",
+        "designation": "Technology Junior Consultant",
+        "phone": "9869123456/0223451235",
+        "image": "img/gentlemenImg/man4.jpg",
+        "DOB": "2012-02-08",
+        "DOJ": "2022-12-21"
+    },
+    {
+        "id": "9",
+        "email": "Emile.wong@cmss.in",
+        "name": "Emile Wong",
+        "dept": "Opreations",
+        "designation": "Global Management Trainee",
+        "phone": "9869123456/0223451235",
+        "image": "img/ladiesImg/lady5.jpg",
+        "DOB": "2002-11-21",
+        "DOJ": "2002-02-21"
+    },
+    {
+        "id": "10",
+        "email": "sohan.yadav@cmss.in",
+        "name": "Sohan Yadav",
+        "dept": "IT",
+        "designation": "IT Service & Support",
+        "phone": "9869123456/0223451235",
+        "image": "img/gentlemenImg/man5.jpg",
+        "DOB": "2001-02-24",
+        "DOJ": "2023-05-15"
+    },
+    {
+        "id": "11",
+        "email": "angela.morris@cmss.in",
+        "name": "Angela Morris",
+        "dept": "HR",
+        "designation": "Senior Manager - HR",
+        "phone": "9869123456/0223451235",
+        "image": "img/ladiesImg/lady6.jpg",
+        "DOB": "2001-01-09",
+        "DOJ": "2022-09-18"
+    },
+    {
+        "id": "12",
+        "email": "loki.ramos@cmss.in",
+        "name": "Loki Ramos",
+        "dept": "Opreations",
+        "designation": "Collections Manager",
+        "phone": "9869123456/0223451235",
+        "image": "img/gentlemenImg/man6.jpg",
+        "DOB": "2001-12-13",
+        "DOJ": "2009-02-03"
+    },
+    {
+        "id": "13",
+        "email": "riya.singh@cmss.in",
+        "name": "Riya Singh",
+         "dept": "IT",
+        "designation": "Technology Consultant",
+        "phone": "9869123456/0223451235",
+        "image": "img/ladiesImg/lady7.jpg",
+        "DOB": "1993-09-12",
+        "DOJ": "2021-02-22"
+    },
+    {
+        "id": "14",
+        "email": "robert.doe@cmss.in",
+        "name": "Robert Doe",
+        "dept": "Accounts",
+        "designation": "Manager-Finance & Accounts",
+        "phone": "9869123456/0223451235",
+        "image": "img/gentlemenImg/man10.jpg",
+        "DOB": "1995-09-08",
+        "DOJ": "2022-03-06"
+    },
+    {
+        "id": "15",
+        "email": "lili.ching@cmss.in",
+        "name": "Lili Ching",
+        "dept": "IT",
+        "designation": "Software Developer",
+        "phone": "9869123456/0223451235",
+        "image": "img/ladiesImg/lady7.jpg",
+        "DOB": "1990-09-25",
+        "DOJ": "2023-07-14"
+    },
+    {
+        "id": "16",
+        "email": "om.chobey@cmss.in",
+        "name": "Om Chobey",
+        "dept": "Marketing",
+        "designation": "Category Head",
+        "phone": "9869123456/0223451235",
+        "image": "img/gentlemenImg/man7.jpg",
+        "DOB": "2002-09-06",
+        "DOJ": "2023-03-12"
+    },
+    {
+        "id": "17",
+        "email": "siri.disuja@cmss.in",
+        "name": "Siri Disuja",
+        "dept": "Opreations",
+        "designation": "Operations Manager",
+        "phone": "9869123456/0223451235",
+        "image": "img/ladiesImg/lady8.jpg",
+        "DOB": "2001-09-15",
+        "DOJ": "2023-04-20"
+    },
+    {
+        "id": "18",
+        "email": "nishi.ramos@cmss.in",
+        "name": "Nishi Ramos",
+        "dept": "Accounts",
+        "designation": "Operations Manager",
+        "phone": "9869123456/0223451235",
+        "image": "img/gentlemenImg/man8.jpg",
+        "DOB": "2004-09-05",
+        "DOJ": "2023-01-29"
+    },
+    {
+        "id": "19",
+        "email": "niki.sen@cmss.in",
+        "name": "Niki Sen",
+        "dept": "Admin",
+        "designation": "Administrative Intern",
+        "phone": "9869123456/0223451235",
+        "image": "img/ladiesImg/lady9.jpg",
+        "DOB": "1999-09-14",
+        "DOJ": "2022-06-19"
+    },
+    {
+        "id": "20",
+        "email": "sanjay.jha@cmss.in",
+        "name": "Sanjay Jha",
+        "dept": "IT",
+        "designation": "Technology Consultant",
+        "phone": "9869123456/0223451235",
+        "image": "img/gentlemenImg/man9.jpg",
+        "DOB": "2001-09-07",
+        "DOJ": "2023-04-12"
+    }
+
+]
+
+// birthday logic
+const get_last_week_data = () => {
+
+    const today = new Date();
+    const lastMonday = new Date(today);
+    lastMonday.setDate(today.getDate() - today.getDay() - 6);
+    const lastSunday = new Date(today);
+    lastSunday.setDate(today.getDate() - today.getDay());
+
+    return data.filter((item) => {
+        let date_of_employe = new Date(item.DOB);
+        let birthday_month = date_of_employe.getMonth();
+        let birthday_date = date_of_employe.getDate();
+        let emplpoyee_date = new Date();
+        if ((birthday_date >= lastMonday.getDate() && birthday_date <= lastSunday) && (birthday_month === lastMonday.getMonth())) {
+            emplpoyee_date.setDate(birthday_date);
+
+        }
+        const itemDate = new Date(emplpoyee_date);
+        return itemDate >= lastMonday && itemDate <= lastSunday;
+    });
+
+};
+
+const get_this_week_data = () => {
+    const today = new Date();
+    const thisWeekStart = new Date(today);
+    thisWeekStart.setDate(today.getDate() - today.getDay() + 1);
+
+    const thisWeekEnd = new Date(today);
+    thisWeekEnd.setDate(today.getDate() - today.getDay() + 7);
+
+    return data.filter(item => {
+        let date_of_employe_thisWeek = new Date(item.DOB);
+        let birthday_month_thisWeek = date_of_employe_thisWeek.getMonth();
+        let birthday_date_thisWeek = date_of_employe_thisWeek.getDate();
+        let emplpoyee_date_thisWeek = new Date();
+        if ((birthday_date_thisWeek >= thisWeekStart.getDate() && birthday_date_thisWeek <= thisWeekEnd.getDate()) && (birthday_month_thisWeek === thisWeekEnd.getMonth())) {
+            emplpoyee_date_thisWeek.setDate(birthday_date_thisWeek);
+
+            const itemDates = new Date(emplpoyee_date_thisWeek);
+
+            return itemDates >= thisWeekStart && itemDates <= thisWeekEnd;
+        }
+
+    });
+
+
+
+};
+
+
+
+const get_upcoming_week_data = () => {
+
+    const today = new Date();
+    const nextWeekStart = new Date(today);
+    nextWeekStart.setDate(today.getDate() + (7 - today.getDay()) + 1);
+    const nextWeekEnd = new Date(today);
+    nextWeekEnd.setDate(today.getDate() + (13 - today.getDay()) + 1);
+
+    return data.filter(item => {
+        let date_of_employe_upcoming = new Date(item.DOB);
+        let birthday_month_upcoming = date_of_employe_upcoming.getMonth();
+        let birthday_date_upcoming = date_of_employe_upcoming.getDate();
+        let emplpoyee_date_upcoming = new Date();
+        if ((birthday_date_upcoming >= nextWeekStart.getDate() && birthday_date_upcoming <= nextWeekEnd.getDate()) && (birthday_month_upcoming === nextWeekEnd.getMonth())) {
+            emplpoyee_date_upcoming.setDate(birthday_date_upcoming)
+
+            const itemDate = new Date(emplpoyee_date_upcoming);
+
+            return itemDate >= nextWeekStart && itemDate <= nextWeekEnd;
+        }
+
+    });
+
+};
+
+let card = document.getElementById("card");
+const lastWeekButton = document.getElementById('last-week');
+const thisWeekButton = document.getElementById('this-week');
+const upcomingButton = document.getElementById('upcoming');
+const carouselInner = $('#birthday-carousel .carousel-inner');
+const carouselPrev = $('#birthday-carousel .carousel-control-prev');
+const carouselNext = $('#birthday-carousel .carousel-control-next');
+
+let currentData = [];
+let currentIndex = 0;
+const cardsPerSlide = 3;
+
+function updateButtonCounts() {
+
+    lastWeekButton.textContent = `Last Week (${get_last_week_data().length})`;
+    thisWeekButton.textContent = `This Week (${get_this_week_data().length})`
+
+    upcomingButton.textContent = `Upcoming (${get_upcoming_week_data().length})`;
+}
+lastWeekButton.addEventListener('click', () => {
+    currentData = get_last_week_data();
+    currentIndex = 0;
+    renderData();
+    updateButtonCounts();
+
+});
+
+thisWeekButton.addEventListener('click', () => {
+    currentData = get_this_week_data();
+    currentIndex = 0;
+    renderData();
+    updateButtonCounts();
+});
+
+upcomingButton.addEventListener('click', () => {
+    currentData = get_upcoming_week_data();
+    currentIndex = 0;
+    renderData();
+    updateButtonCounts();
+
+});
+function renderData() {
+    carouselInner.empty();
+    const currentChunk = currentData.slice(currentIndex, currentIndex + cardsPerSlide);
+    if (currentChunk.length === 0) {
+        carouselInner.append('<div class="carousel-item active"><p>No birthdays found.</p></div>');
+       
+    }
+    else {
+        let activeClass = 'active';
+        const birthdayRow = $('<div>').addClass(`carousel-item ${activeClass}`);
+         
+        const rowContainer = $('<div>').addClass('row');
+        for (const employee of currentChunk) {
+            const birthdayCol = $('<div>').addClass('col-md-4');
+            let backColor=["#74cdcd","#e09a19","#c2ff84","blue","purple"];
+            console.log(backColor[Math.round(Math.random())]);
+                birthdayCol.html(`
+                <div class="birthday-card" style="background-color:${backColor[Math.round(Math.random())]}">
+                    <p class="font-weight-bold mb-2 date">${employee.DOB}</p>
+                    <img class="birthdayImg" src="${employee.image}"/>
+                    <h6 class="font-weight-bold mt-3 mb-1" style="    font-size: 18px;">${employee.name}</h6>
+                    <p class="designation">${employee.designation}</p>
+                </div>
+            `);
+           
+            
+            rowContainer.append(birthdayCol);
+        }
+        birthdayRow.append(rowContainer);
+        carouselInner.append(birthdayRow);
+        activeClass = '';
+     
+    }
+
+
+}
+carouselPrev.click(function () {
+    if (currentIndex >= cardsPerSlide) {
+        currentIndex -= cardsPerSlide;
+        console.log(currentIndex, cardsPerSlide);
+        renderData();
+
+    }
+});
+carouselNext.click(function () {
+    if (currentIndex + cardsPerSlide < currentData.length) {
+        currentIndex += cardsPerSlide;
+        console.log(currentIndex, cardsPerSlide);
+        renderData();
+    }
+});
+
+// Initialize with This Week's birthdays
+thisWeekButton.click();
+
+
+//new joinee logic
+
+const employeeCarousel = $('#employee-carousel');
+const carouselInner_employee = $('#employee-carousel .carousel-inner');
+const carouselPrev_employee = $('#employee-carousel .carousel-control-prev');
+const carouselNext_employee = $('#employee-carousel .carousel-control-next');
+console.log(carouselInner_employee,carouselPrev_employee,carouselNext_employee);
+// Filter employees who joined in the latest year
+const latestYear = new Date().getFullYear();
+let latest_newJOinees = data.filter(function (employee) {
+    const joining_year = new Date(employee.DOJ).getFullYear();
+    return joining_year === latestYear;
+});
+let currentIndexForEmployee = 0;
+const employeesPerSlide = 3;
+
+function renderEmployee() {
+    carouselInner_employee.empty();
+    const currentChunks = latest_newJOinees.slice(currentIndexForEmployee, currentIndexForEmployee + employeesPerSlide);
+    if (currentChunks.length === 0) {
+        carouselInner_employee.append('<div class="carousel-item active"><p>No employees found.</p></div>');
+     
+    }
+    else {
+        let activeClass = 'active';
+        const employeeRow = $('<div>').addClass(`carousel-item ${activeClass}`);
+        const rowContainer = $('<div>').addClass('row');
+        for (const employee of currentChunks) {
+            const employeeCol = $('<div>').addClass('col-md-4');
+            employeeCol.html(`
+            <div class="employee-card ">
+               <p class="font-weight-bold mb-2 date">${employee.DOJ}</p>
+              <img class="employeeImg" src="${employee.image}"/>
+                <h6 class="font-weight-bold mt-3 mb-1" style="    font-size: 18px;">${employee.name}</h6>
+                <p class="designation">${employee.designation}</p>
+            </div>
+        `);
+            rowContainer.append(employeeCol);
+        }
+        employeeRow.append(rowContainer);
+        carouselInner_employee.append(employeeRow);
+        activeClass = '';
+        // if (latest_newJOinees.length > currentIndex + employeesPerSlide) {
+        //     carouselPrev_employee.show();
+        //     carouselNext_employee.show();
+        // }
+        // else {
+        //     carouselPrev_employee.hide();
+        //     carouselNext_employee.hide();
+        // }
+    }
+}
+carouselPrev_employee.click(function () {
+    if (currentIndexForEmployee >= employeesPerSlide) {
+        currentIndexForEmployee -= employeesPerSlide;
+        renderEmployee();
+    }
+});
+carouselNext_employee.click(function () {
+    if (currentIndexForEmployee + employeesPerSlide < latest_newJOinees.length) {
+        currentIndexForEmployee += employeesPerSlide;
+        renderEmployee();
+    }
+});
+
+renderEmployee();
+
+//table and serch logic
+
+const itemsPerPage_table = 10;
+let currentPage_table = 1;
+
+// Initial table population and pagination setup
+const initialFilter = { name: '', dept: '', id: Number(''), designation: '' };
+populateTable(currentPage_table, initialFilter);
+updatePagination(initialFilter);
+
+// Search input event handlers
+    
+$('#nameInput').on('input', () => {
+    const filter = {
+        name: $('#nameInput').val(),
+        dept: $('#departmentInput').val(),
+        id: Number($('#idInput').val()),
+        designation: $('#designationInput').val(),
+    };
+    currentPage = 1;
+    populateTable(currentPage_table, filter);
+    updatePagination(filter);
+    
+});
+$('#departmentInput').on('input', () => {
+    const filter = {
+        name: $('#nameInput').val(),
+        dept: $('#departmentInput').val(),
+        id: Number($('#idInput').val()),
+        designation: $('#designationInput').val(),
+    };
+    currentPage_table = 1;
+    populateTable(currentPage_table, filter);
+    updatePagination(filter);
+});
+
+$('#idInput').on('input', () => {
+    const filter = {
+        name: $('#nameInput').val(),
+        dept: $('#departmentInput').val(),
+        id:Number($('#idInput').val()),
+        designation: $('#designationInput').val(),
+    };
+    currentPage_table = 1;
+    populateTable(currentPage_table, filter);
+    updatePagination(filter);
+});
+
+$('#designationInput').on('input', () => {
+    const filter = {
+        name: $('#nameInput').val(),
+        dept: $('#departmentInput').val(),
+        id: Number($('#idInput').val()),
+        designation: $('#designationInput').val(),
+    };
+    currentPage_table = 1;
+    populateTable(currentPage_table, filter);
+    updatePagination(filter);
+});
+
+// Function to populate the table with  data
+function populateTable(page, filter) {
+    const startIndex = (page - 1) * itemsPerPage_table;
+    let filteredData = data;
+ 
+       // Apply filters
+       if (filter.name) {
+        filteredData = filteredData.filter(employee => employee.name.toLowerCase().includes(filter.name.toLowerCase()));
+      }
+      if (filter.dept) {
+        filteredData = filteredData.filter(employee => employee.dept.toLowerCase().includes(filter.dept.toLowerCase()));
+    }
+    if (filter.id) {
+        filteredData = filteredData.filter(employee => employee.id.includes(filter.id));
+    }
+    if (filter.designation) {
+        filteredData = filteredData.filter(employee => employee.designation.toLowerCase().includes(filter.designation.toLowerCase()));
+    }
+    const endIndex = startIndex + itemsPerPage_table;
+    const paginatedData = filteredData.slice(startIndex, endIndex);
+    const tableBody = $('#tableBody');
+    tableBody.empty();
+
+    paginatedData.forEach(employee => {
+        tableBody.append(`
+            <tr>
+                <td><img class="employeeTableImg" src='${employee.image}'/></td>
+                <td>${employee.name}</td>
+                <td>${employee.dept}</td>
+                <td>${employee.email}</td>
+                <td>${employee.designation}</td>
+                <td>${employee.phone}</td>
+            </tr>
+        `);
+    });
+
+   
+}
+// Function to update pagination
+function updatePagination(filter) {
+    let filteredData = data;
+    // Apply filters
+    if (filter.name) {
+        filteredData = filteredData.filter(employee => employee.name.toLowerCase().includes(filter.name.toLowerCase()));
+    }
+    if (filter.dept) {
+        filteredData = filteredData.filter(employee => employee.dept.toLowerCase().includes(filter.dept.toLowerCase()));
+    }
+    if (filter.id) {
+        filteredData = filteredData.filter(employee => employee.id.includes(filter.id));
+    }
+    if (filter.designation) {
+        filteredData = filteredData.filter(employee => employee.designation.toLowerCase().includes(filter.designation.toLowerCase()));
+    }
+    const totalPages = Math.ceil(filteredData.length / itemsPerPage_table);
+    const pagination = $('#pagination');
+    pagination.empty();
+
+    for (let i = 1; i <= totalPages; i++) {
+        const li = $('<li class="page-item"><a class="page-link" href="#">' + i + '</a></li>');
+        li.click(() => {
+            currentPage_table = i;
+            populateTable(currentPage_table, filter);
+            updatePagination(filter);
+        });
+        pagination.append(li);
+    }
+}
+
+
+
+
+
